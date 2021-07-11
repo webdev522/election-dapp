a8624e83e116d230b5150d060ca5bcc633429d66
webdev522 <webdev522@gmail.com>
2021-07-11 17:59:30 +0900

    implement frontend

--- src/js/app.js
+++ src/js/app.js
@@ -1,66 +1,144 @@
-App = {
-  web3Provider: null,
-  contracts: {},
-  account: "0x0",
+var web3
+var contractAddress
+var abi
+var contract
+var account
+var loader
+var content
 
-  init: function () {
-    return App.initWeb3()
-  },
+function init() {
+  web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545/"))
 
-  initWeb3: function () {
-    if (typeof web3 !== "undefined") {
-      // If a web3 instance is already provided by Meta Mask.
-      App.web3Provider = web3.currentProvider
-      web3 = new Web3(web3.currentProvider)
-    } else {
-      // Specify default instance if no web3 instance provided
-      App.web3Provider = new Web3.providers.HttpProvider(
-        "http://localhost:7545"
-      )
-      web3 = new Web3(App.web3Provider)
+  // contractAddress and abi are setted after contract deploy
+  contractAddress = "0x95014228dDF28ceBdDB35C8ae1dd73F517eC3F42"
+  abi = [
+    {
+      constant: true,
+      inputs: [],
+      name: "candidatesCount",
+      outputs: [
+        {
+          name: "",
+          type: "uint256",
+        },
+      ],
+      payable: false,
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      constant: true,
+      inputs: [
+        {
+          name: "",
+          type: "uint256",
+        },
+      ],
+      name: "candidates",
+      outputs: [
+        {
+          name: "id",
+          type: "uint256",
+        },
+        {
+          name: "name",
+          type: "string",
+        },
+        {
+          name: "voteCount",
+          type: "uint256",
+        },
+      ],
+      payable: false,
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      constant: true,
+      inputs: [
+        {
+          name: "",
+          type: "address",
+        },
+      ],
+      name: "voters",
+      outputs: [
+        {
+          name: "",
+          type: "bool",
+        },
+      ],
+      payable: false,
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [],
+      payable: false,
+      stateMutability: "nonpayable",
+      type: "constructor",
+    },
+    {
+      constant: false,
+      inputs: [
+        {
+          name: "_candidateId",
+          type: "uint256",
+        },
+      ],
+      name: "vote",
+      outputs: [],
+      payable: false,
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+  ]
+
+  //contract instance
+  contract = new web3.eth.Contract(abi, contractAddress)
+  console.log("hello", contract)
+
+  web3.eth.getAccounts(function (err, accounts) {
+    if (err != null) {
+      alert("Error retrieving accounts.")
+      return
     }
-    return App.initContract()
-  },
+    if (accounts.length == 0) {
+      alert(
+        "No account found! Make sure the Ethereum client is configured properly."
+      )
+      return
+    }
+    account = accounts[0]
+    web3.eth.defaultAccount = account
+    $("#accountAddress").html("Your Account: " + account)
+  })
 
-  initContract: function () {
-    $.getJSON("Election.json", function (election) {
-      // Instantiate a new truffle contract from the artifact
-      App.contracts.Election = TruffleContract(election)
-      // Connect provider to interact with contract
-      App.contracts.Election.setProvider(App.web3Provider)
+  loader = $("#loader")
+  content = $("#content")
 
-      return App.render()
-    })
-  },
+  loader.show()
+  content.hide()
 
-  render: function () {
-    var electionInstance
-    var loader = $("#loader")
-    var content = $("#content")
+  reload()
+}
 
-    loader.show()
-    content.hide()
+function reload() {
+  contract.methods
+    .candidatesCount()
+    .call()
+    .then(function (candidatesCount) {
+      var candidatesResults = $("#candidatesResults")
+      candidatesResults.empty()
 
-    // Load account data
-    web3.eth.getCoinbase(function (err, account) {
-      if (err === null) {
-        App.account = account
-        $("#accountAddress").html("Your Account: " + account)
-      }
-    })
+      var candidatesSelect = $("#candidatesSelect")
+      candidatesSelect.empty()
 
-    // Load contract data
-    App.contracts.Election.deployed()
-      .then(function (instance) {
-        electionInstance = instance
-        return electionInstance.candidatesCount()
-      })
-      .then(function (candidatesCount) {
-        var candidatesResults = $("#candidatesResults")
-        candidatesResults.empty()
-
-        for (var i = 1; i <= candidatesCount; i++) {
-          electionInstance.candidates(i).then(function (candidate) {
+      for (var i = 1; i <= candidatesCount; i++) {
+        contract.methods
+          .candidates(i)
+          .call()
+          .then(function (candidate) {
             var id = candidate[0]
             var name = candidate[1]
             var voteCount = candidate[2]
@@ -75,20 +153,54 @@ App = {
               voteCount +
               "</td></tr>"
             candidatesResults.append(candidateTemplate)
-          })
-        }
 
-        loader.hide()
-        content.show()
-      })
-      .catch(function (error) {
-        console.warn(error)
-      })
-  },
+            // Render candidate ballot option
+            var candidateOption =
+              "<option value='" + id + "' >" + name + "</ option>"
+            candidatesSelect.append(candidateOption)
+          })
+      }
+
+      loader.hide()
+      content.show()
+
+      contract.methods
+        .voters(account)
+        .call()
+        .then(function (result) {
+          console.log(result)
+          if (result) {
+            $("#btn-vote").prop("disabled", true)
+          } else {
+            $("#btn-vote").prop("disabled", false)
+          }
+        })
+        .catch(function (error) {
+          console.log("error")
+        })
+    })
+}
+
+function castVote() {
+  loader.show()
+  content.hide()
+  var candidateId = $("#candidatesSelect").val()
+  contract.methods
+    .vote(candidateId)
+    .send({ from: account })
+    .then(function () {
+      reload()
+    })
+    .catch(function (err) {
+      console.error(err)
+      alert("Can't vote anymore")
+      loader.hide()
+      content.show()
+    })
 }
 
 $(function () {
   $(window).load(function () {
-    App.init()
+    init()
   })
 })
